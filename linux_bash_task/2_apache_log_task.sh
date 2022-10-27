43dc30aafacbccc6f143ba43d6c1787283cbb434
max <max@mint1.local>
2022-10-28 00:21:53 +0300

    working on linux bash task 2 punkt 5

--- linux_bash_task/2_apache_log_task.sh
+++ linux_bash_task/2_apache_log_task.sh
@@ -17,22 +17,58 @@ if [ -e $logfile ]; then
 
         case $option in
 
-        1) cat $logfile | awk '{ print $1 }' | uniq -c | sort -nr | head -5
-           ;; 
-        
-        2) cat $logfile | awk '{ print $7 }' | uniq -c | sort -nr | head -3
-            ;;
+            1) cat $logfile | awk '{ print $1 }' | uniq -c | sort -nr | head -5
+                ;; 
+            
+            2) cat $logfile | awk '{ print $7 }' | uniq -c | sort -nr | head -3
+                ;;
 
-        3) cat $logfile | awk '{ print $1 }' | uniq -c | sort -nr
+            3) cat $logfile | awk '{ print $1 }' | uniq -c | sort -nr | awk '{ print $1 " " "requests" " " "from:" " " $2 }'
+                ;;
+
+            4) printf "Non-existent pages referred by clients: %s\n\n"
+
+            cat $logfile | awk '{ print $9,$7 }' | grep "404" | awk '{ print $2 }' | tee file
+
+            printf "%s\nTotal: $(cat file | wc -l) files %s\n"
+
+            rm -f file
+                ;;
+
+            5) requests () {
+
+                    printf "between 8:00 and 9:00:\t"
+                    
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:08./ p' | printf "`wc -l` requests\n"
+
+                    printf "between 9:00 and 10:00\t"
+
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:09./ p' | printf "`wc -l` requests\n"
+
+                    printf "between 10:00 and 11:00\t"
+
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:10./ p' | printf "`wc -l` requests\n"
+
+                    printf "between 11:00 and 12:00\t"
+
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:11./ p' | printf "`wc -l` requests\n"
+
+                    printf "between 12:00 and 13:00\t"
+
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:12./ p' | printf "`wc -l` requests\n"
+
+                    printf "between 13:00 and 14:00\t"
+
+                    cat $logfile | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:13./ p' | printf "`wc -l` requests\n"
+
+                }
+                topreq=$(requests | awk '{print $5}' | sort -nr | sed -n '1 p')
+
+                requests | sed -n "/$topreq/p" | sed -n 's/^/Site got most requests /p' | sed -n 's/and [0-9][0-9]\:00/&\-/p'
+                ;; 
+
 
         esac
-
-
-
-
-
-
-
 
 
 
--- linux_bash_task/test1.sh
+++ linux_bash_task/test1.sh
@@ -2,4 +2,32 @@
 
 file=./example_log.log
 
-cat $file | awk '{ print $7 }' | uniq -c | sort -nr | head -5
+requests () {
+    printf "between 8:00 and 9:00:\t"
+    
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:08./ p' | printf "`wc -l` requests\n"
+
+    printf "between 9:00 and 10:00\t"
+
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:09./ p' | printf "`wc -l` requests\n"
+
+    printf "between 10:00 and 11:00\t"
+
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:10./ p' | printf "`wc -l` requests\n"
+
+    printf "between 11:00 and 12:00\t"
+
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:11./ p' | printf "`wc -l` requests\n"
+
+    printf "between 12:00 and 13:00\t"
+
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:12./ p' | printf "`wc -l` requests\n"
+
+    printf "between 13:00 and 14:00\t"
+
+    cat $file | awk '{print $4}' | sed 's/\[//g' | sed -n '/25\/Apr\/2017\:13./ p' | printf "`wc -l` requests\n"
+
+}
+topreq=$(requests | awk '{print $5}' | sort -nr | sed -n '1 p')
+
+requests | sed -n "/$topreq/p" | sed -n 's/^/Site got most requests /p' | sed -n 's/and [0-9][0-9]\:00/&\-/p'
